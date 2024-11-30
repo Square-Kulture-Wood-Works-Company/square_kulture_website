bdf391307e46f1b828912da2fa2e9b7a1a0dd357
sudobigtek <ugoemmanuel13@gmail.com>
2024-11-30 09:26:55 +0100

    tried to change the website template

--- java.js
+++
@@ -1,8 +0,0 @@
-const ws = new WebSocket('ws://example.com/socket');
-ws.onopen = function() {
-  console.log('Connection established');
-  ws.send('Hello, server!');
-};
-ws.onmessage = function(event) {
-  console.log('Received message: ' + event.data);
-};
